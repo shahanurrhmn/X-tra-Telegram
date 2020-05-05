aae2f804741b3ba31bd5163ea949278e2c738ab5
SHAHANUR  ALI AHMED <35138607+shahanurrhmn@users.noreply.github.com>
2020-05-05 20:46:25 +0530

    Uploaded New Plugin

---
+++ userbot/plugins/heart.py
@@ -0,0 +1,14 @@
+from telethon import events
+import asyncio
+from collections import deque
+
+
+@borg.on(events.NewMessage(pattern=r"\.heart", outgoing=True))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("❤️🧡💛💚💙💜🖤"))
+	for _ in range(48):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
